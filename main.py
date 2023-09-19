6f25326137d175011cce92536db370a31b46ddb4
Swaon <swaon@hotmail.com>
2023-09-19 17:26:11 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+from settings import *
+import requests
+
+
+def wakfu_version():
+	response_version = requests.get(VERSION_URL)
+	if response_version.status_code == 200:
+		config_json = json.loads(response_version.content)
+		version = config_json["version"]
+		return version
+	else:
+		print("Error config JSON")
+		return None
+
+def main():
+	version = wakfu_version()
+	url = DATA_URL.replace("<version>", version).replace("<type>", type["name"])
+
+if __name__ == "__main__":
+	main()
---
+++ settings.py
@@ -0,0 +1,2 @@
+DATA_URL = "https://wakfu.cdn.ankama.com/gamedata/<version>/<type>.json"
+VERSION_URL = "https://wakfu.cdn.ankama.com/gamedata/config.json"
