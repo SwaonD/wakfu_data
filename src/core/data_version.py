064e4b8293786b0a799afefd5e8d75b8d0d2e0cc
Swaon <swaon@hotmail.com>
2023-09-20 18:59:06 +0200

    fix imports, add custom data

--- src/core/data_version.py
+++ src/core/data_version.py
@@ -1,5 +1,5 @@
-from settings import VERSION_URL
-from tools.json_tools import get_json_data_with_url
+from .settings import VERSION_URL
+from .tools.json_tools import get_json_data_with_url
 
 def get_version():
 	data = get_json_data_with_url(VERSION_URL)
