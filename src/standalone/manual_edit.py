ed7b624ed856608016b4eff9a5c85dde082de2e1
Swaon <swaon@hotmail.com>
2023-09-28 17:14:03 +0200

    zones : name to id

--- src/standalone/manual_edit.py
+++ src/standalone/manual_edit.py
@@ -6,20 +6,9 @@ from core.tools.json_tools import get_json_data_with_path, write_json_in_copy, \
 		put_key_at_position, get_reordered_keys
 from core.settings import CUSTOM_DATA_PATH
 
+zones_file = CUSTOM_DATA_PATH / "zones.json"
+zones_data = get_json_data_with_path(zones_file)
+# edit data here
 
-zoneNames_file = CUSTOM_DATA_PATH / "zoneNames.json"
-zoneNames_data = get_json_data_with_path(zoneNames_file)
 # edit data here
-i = 0
-while i < len(zoneNames_data):
-	y = 0
-	while y < len(zoneNames_data):
-		if y == i:
-			y += 1
-			continue
-		if zoneNames_data[i]["title"]["fr"] == zoneNames_data[y]["title"]["fr"]:
-			print(f'{zoneNames_data[i]["title"]["fr"]} en double')
-		y += 1
-	i += 1
-# edit data here
-# write_json_in_copy(zones_file, zones_data)
+write_json_in_copy(zones_file, zones_data)
